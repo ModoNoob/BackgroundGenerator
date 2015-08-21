adfc9e866b1e439f910fd6837a607e5d62c78823
ModoNoob <ModoNoob@hotmail.fr>
2015-08-21 15:37:26 -0400

    Added Application Arguments
    Added application arguments so that the variables are not hard-coded
    anymore.

--- src/net/modonoob/backgroundgenerator/BackgroundCreator.java
+++ src/net/modonoob/backgroundgenerator/BackgroundCreator.java
@@ -19,8 +19,27 @@ public class BackgroundCreator {
     private static Triangle[] triangles;
     
     public static void main(String[] args) {
-        createRendersFolder();
-        init();
+        //Example usage: java -jar BackgroundGenerator 1920 1080 5 Renders/
+        if(args.length > 4 || args.length < 4) {
+            System.out.println("Needed arguments: [desktop width] [desktop height] [number of image to generate] [output directory]");
+            System.out.println("For desktop width and height, you can specify something like \"1920x2\" if you have two horizontal monitors.");
+            System.exit(0);
+        }
+        
+        System.out.println("Initializing BackgroundGenerator...");
+        width = parseSize(args[0]);
+        height = parseSize(args[1]);
+        imageQty = Integer.parseInt(args[2]);
+        System.out.println("Width: " + width + ", Height: " + height + ", Number of images to generate: " + imageQty);
+        bg = new BackgroundGenerator(width, height);
+        
+        createRendersFolder(args[3]);
+        
+        for(int i = 1; i <= imageQty; i++) {
+            System.out.println("Creating image " + i + " of " + imageQty + "...");
+            triangles = bg.generate();
+            createImage();
+        }
     }
     
     private static void createImage() {
@@ -63,25 +82,21 @@ public class BackgroundCreator {
         System.out.println("\tCreated image " + DIRECTORY + d.getTime() + ".png");
     }
     
-    private static void createRendersFolder() {
-        File d = new File(DIRECTORY);
+    private static void createRendersFolder(String path) {
+        File d = new File(path);
         if(!d.exists()) {
             d.mkdir();
             System.out.println("Created folder " + d.getAbsolutePath());
         }
     }
-
-    public static void init() {
-        System.out.println("Initializing BackgroundGenerator...");
-        width = 1920 * 2;
-        height = 1080;
-        imageQty = 5;
-        bg = new BackgroundGenerator(width, height);
-        for(int i = 1; i <= imageQty; i++) {
-            System.out.println("Creating image " + i + " of " + imageQty + "...");
-            triangles = bg.generate();
-            createImage();
+    
+    private static int parseSize(String s) {
+        if(s.contains("x")) {
+            String splittedString[] = s.split("x");
+            int size = Integer.parseInt(splittedString[0]);
+            int qty = Integer.parseInt(splittedString[1]);
+            return size * qty;
         }
-        System.exit(0);
+        return Integer.parseInt(s);
     }
 }
